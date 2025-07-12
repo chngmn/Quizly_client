49816837f5c05a79c87f2d880ef5d2b803ae8e0a
Changmin Lee <107976623+chngmn@users.noreply.github.com>
2025-07-12 15:55:43 +0900

    feat: 로그인하지 않을 시 페이지들 접근 불가능하도록 구현

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import LoginPage from './pages/LoginPage';
 import KakaoRedirectPage from './pages/KakaoRedirectPage';
 import MainPage from './pages/MainPage';
 import QuizPage from './pages/QuizPage';
-import QuizUploadPage from './pages/QuizUploadPage'
+import QuizUploadPage from './pages/QuizUploadPage';
 import WrongAnsPage from './pages/WrongAnsPage';
 import MyQuizzesPage from './pages/MyQuizzesPage';
 import NoticePage from './pages/NoticePage';
 import MyinfoPage from './pages/MyinfoPage';
+import ProtectedRoute from './components/ProtectedRoute'; // ProtectedRoute 임포트
 
 function App() {
   return (
@@ -18,13 +19,17 @@ function App() {
         <Route path="/" element={<StartPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/oauth" element={<KakaoRedirectPage />} />
-        <Route path="/main" element={<MainPage />} />
-        <Route path="/quiz" element={<QuizPage />} />
-        <Route path="/quiz-upload" element={<QuizUploadPage />} />
-        <Route path="/wrong-answers" element={<WrongAnsPage />} />
-        <Route path="/my-quizzes" element={<MyQuizzesPage />} />
-        <Route path="/notice" element={<NoticePage />} />
-        <Route path="/my-info" element={<MyinfoPage />} />
+
+        {/* 로그인 보호가 필요한 라우트들 */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/main" element={<MainPage />} />
+          <Route path="/quiz" element={<QuizPage />} />
+          <Route path="/quiz-upload" element={<QuizUploadPage />} />
+          <Route path="/wrong-answers" element={<WrongAnsPage />} />
+          <Route path="/my-quizzes" element={<MyQuizzesPage />} />
+          <Route path="/notice" element={<NoticePage />} />
+          <Route path="/my-info" element={<MyinfoPage />} />
+        </Route>
       </Routes>
     </Router>
   );
