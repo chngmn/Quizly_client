afb55c8ff3a0736f0baa512468215f66e8026fae
Changmin Lee <107976623+chngmn@users.noreply.github.com>
2025-07-12 15:41:58 +0900

    design: Navbar 구현 및 각 화면 page 연결

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import StartPage from './pages/StartPage';
 import LoginPage from './pages/LoginPage';
-
 import KakaoRedirectPage from './pages/KakaoRedirectPage';
 import MainPage from './pages/MainPage';
+import QuizPage from './pages/QuizPage';
+import QuizUploadPage from './pages/QuizUploadPage'
+import WrongAnsPage from './pages/WrongAnsPage';
+import MyQuizzesPage from './pages/MyQuizzesPage';
+import NoticePage from './pages/NoticePage';
+import MyinfoPage from './pages/MyinfoPage';
 
 function App() {
   return (
@@ -14,6 +19,12 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/oauth" element={<KakaoRedirectPage />} />
         <Route path="/main" element={<MainPage />} />
+        <Route path="/quiz" element={<QuizPage />} />
+        <Route path="/quiz-upload" element={<QuizUploadPage />} />
+        <Route path="/wrong-answers" element={<WrongAnsPage />} />
+        <Route path="/my-quizzes" element={<MyQuizzesPage />} />
+        <Route path="/notice" element={<NoticePage />} />
+        <Route path="/my-info" element={<MyinfoPage />} />
       </Routes>
     </Router>
   );
